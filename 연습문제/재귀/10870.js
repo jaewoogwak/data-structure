be3bbb648b56ecd75703840dda53fb1f09e6242c
Jaewoo Gwak <jaewoo010207@gmail.com>
2022-08-24 19:02:02 +0900

    피보나치 수 5

---
+++ 연습문제/재귀/10870.js
@@ -0,0 +1,18 @@
+let input = require("fs")
+  .readFileSync("예제.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = parseInt(input[0].split(" ")[0]);
+
+const answer = Fibonacci(N);
+console.log(answer);
+
+function Fibonacci(num) {
+  // 기저조건
+  if (num == 0) return 0;
+  else if (num == 1) return 1;
+  // 재귀조건
+  else return Fibonacci(num - 1) + Fibonacci(num - 2);
+}
