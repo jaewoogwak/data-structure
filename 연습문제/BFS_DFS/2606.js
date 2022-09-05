36afd7a08448a7d6def792d3de7fd475bede5696
Jaewoo Gwak <jaewoo010207@gmail.com>
2022-09-06 01:32:39 +0900

    바이러스

---
+++ 연습문제/BFS_DFS/2606.js
@@ -0,0 +1,54 @@
+let input = require("fs")
+  .readFileSync("예제.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+function BFS(graph, startNode) {
+  let visited = []; // 방문 완료한 노드
+  let needVisited = []; // 아직 방문하지 않은 노드
+
+  needVisited.push(startNode); // 첫 노드부터 탐색 시작
+  while (needVisited.length !== 0) {
+    // 방문하지 않은 노드가 있다면
+    const node = needVisited.shift();
+    if (!visited.includes(node)) {
+      // 해당 노드가 방문한 노드가 아니라면
+      visited.push(node);
+      needVisited = [...needVisited, ...graph.AdjList.get(node)];
+    }
+  }
+  return visited;
+}
+
+class Graph {
+  constructor(noOfVertices) {
+    this.noOfVertices = noOfVertices; // 정점의 갯수
+    this.AdjList = new Map(); // 정점의 인접 리스트 저장
+  }
+  addVertex(v) {
+    this.AdjList.set(v, []);
+  }
+  addEdge(v, w) {
+    // 정점 v에 대한 인접 리스트를 얻고, 리스트에 정점 v에 연결된 정점 w를 넣음. (인접 리스트에 추가)
+    this.AdjList.get(v).push(w);
+
+    // 무방향 그래프일경우 v->w, w->v 모두 해주기
+    this.AdjList.get(w).push(v);
+  }
+}
+
+let N = parseInt(input[0]);
+let E = parseInt(input[1]);
+let g = new Graph(N);
+
+for (let i = 1; i < N + 1; i++) {
+  g.addVertex(i);
+}
+// 간선 추가
+for (let m = 0; m < E; m++) {
+  let [i, j] = input[m + 2].split(" ");
+  g.addEdge(parseInt(i), parseInt(j));
+}
+
+console.log(BFS(g, 1).length - 1);
