593fbc7b7e75ca4275a73516983b01ccde415838
Jaewoo Gwak <jaewoo010207@gmail.com>
2022-10-10 15:56:50 +0900

    연결된 원형 큐

---
+++ 자료구조및실습/week06/CircularLinkedQueue.py
@@ -0,0 +1,69 @@
+class CircularLinkedQueue:
+    def __init__(self):
+        self.tail = None
+
+
+    def isEmpty(self):
+        return self.tail == None
+
+
+    def clear(self):
+        self.tail = None
+
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.tail.link.data
+
+
+    def enqueue(self, item):
+        node = Node(item, None)
+        if self.isEmpty():
+            node.link = node
+            self.tail = node
+        
+        else:
+            node.link = self.tail.link
+            self.tail.link = node
+            self.tail = node
+
+
+    def dequeue(self):
+        # dequeue라서 tail을 건드릴 필요가 없다.
+        if not self.isEmpty():
+            data = self.tail.link.data
+            if self.tail.link == self.tail:
+                self.tail = None
+
+            else:
+                self.tail.link = self.tail.link.link
+
+            return data
+
+    
+    def size(self):
+        if self.isEmpty(): return 0
+        else:
+            count = 1
+            node = self.tail.link
+            while not node == self.tail:
+                node = node.link
+                count += 1
+
+            return count
+
+    
+    def display(self):
+        node = self.tail.link
+        while not node == self.tail:
+            print(node.data, end='->')
+            node = node.link
+        print(node.data)
+
+q = CircularLinkedQueue()
+for i in range(8): q.enqueue(i)
+q.display() # 0 1 2 3 4 5 6 7 
+for i in range(5): q.dequeue()
+q.display() # 5 6 7 
+for i in range(8,14): q.enqueue(i)
+q.display() # 5 6 7 8 9 10 11 12 13
